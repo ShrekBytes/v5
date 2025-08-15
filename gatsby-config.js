41c4ebbc443de42e9b6d043fb89eeabf42593eb8
Walid <132557568+ShrekBytes@users.noreply.github.com>
2025-08-15 22:04:16 +0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Walid Ahmed (ShrekBytes)',
     description: 'Walid Ahmed is a backend developer and security tester.',
-    siteUrl: 'https://github.com/ShrekBytes', // No trailing slash allowed!
+    siteUrl: 'https://shrekbytes.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@ShrekBytes',
   },
