f0992612e2a96efb7e8b67b8d8642605677d25b8
Walid <132557568+ShrekBytes@users.noreply.github.com>
2025-08-16 01:35:40 +0600

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,14 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        Your ideas deserve to come to life.
+        <br />
+        Send me a message and let’s make it happen!
+        <br />
+        <br />
+        Feel free to reach out with any questions or just to say hi.
+        <br />
+        And if you don’t want to say hi, you can always...
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
