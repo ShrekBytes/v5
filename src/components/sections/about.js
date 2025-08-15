26ee5ca12b4e22947fea637cc608cda902878581
Walid <132557568+ShrekBytes@users.noreply.github.com>
2025-08-15 14:50:39 +0600

    hero update

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,30 +135,15 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! I’m Walid, currently pursuing my Bachelor's Degree in Computer Science &
+              Engineering. I have experience in backend development and have conducted web security
+              testing, including participation in bug bounty programs.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              I enjoy building products that genuinely interest me, projects I feel I would
+              personally use. In the field of security testing, I enjoy leveraging my attention to
+              detail and analytical mindset to identify vulnerabilities and improve system safety.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,28 +60,21 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Walid Ahmed.</h2>;
+  const three = <h3 className="big-heading">I build things that interest me.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I am actively developing my skills in <b>backend development</b> and <b>security testing</b>
+        . I enjoy exploring new technologies and learning continuously, it keeps me curious. Outside
+        of coding, I like solving puzzles, watching movies, gaming, making memes, and learning new
+        things.
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    <a className="email-link" href="mailto:shrekbytes@duck.com" target="_blank" rel="noreferrer">
+      Get In Touch
     </a>
   );
 
